cbeaaa549b41abab74502c03c42d7ebcd2a19bb5
Jacob <weaver.jacob.t@gmail.com>
2023-09-21 19:19:13 -0500

    blah

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution here
+
+
+
+function findMatching (drivers, string) {
+      return drivers.filter(match => match.toLowerCase() == string.toLowerCase())
+  
+}
+
+
+function fuzzyMatch (drivers, string) {
+  return drivers.filter(function (match) {
+    let slicedComp = match.slice(0,2);
+    if (slicedComp == string)
+    {
+      return match;
+    }
+  })
+
+}
+
+function matchName (drivers, string) {
+  return drivers.filter(match => match.name == string)
+
+}
